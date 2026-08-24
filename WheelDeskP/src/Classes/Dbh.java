b605883cbd3a3ffcd90a6e312d57a872d124fe44
thabang-m-modiba <thabangmamoloko8@gmail.com>
2026-08-24 21:06:58 +0200

    Update Dbh.java
    x

--- WheelDeskP/src/Classes/Dbh.java
+++ WheelDeskP/src/Classes/Dbh.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 public class Dbh {
 	private static String URL = "jdbc:mysql://localhost:3306/wheeldesk";
 	private static String USER = "root";
-	private static String PASSWORD = "#Palaentologist33@sql";
+	private static String PASSWORD = "";
 	
 	public static Connection getConnection() throws SQLException{
 		return DriverManager.getConnection(URL, USER, PASSWORD);
